9a0f504e12b73d9b2b01c6979d35eaf86c88a398
Diego Almeida <D_M_A-@outlook.com>
2022-08-08 02:43:41 -0300

    se hizo conexion con base de datos, se hizo funciones para la primera y cuarta ventana

--- src/ventanas/Ventana_Uno.java
+++ src/ventanas/Ventana_Uno.java
@@ -3,18 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package ventanas;
+import clases.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author Dieguillo
- */
 public class Ventana_Uno extends javax.swing.JFrame {
-
-    /**
-     * Creates new form Ventana_Uno
-     */
+    public static String user = "";
+    public static String pass = "";
     public Ventana_Uno() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -26,36 +30,32 @@ public class Ventana_Uno extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         txt_User = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         pass_Con = new javax.swing.JPasswordField();
         btn_Inicio = new javax.swing.JButton();
-
-        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Background Login.jpeg"))); // NOI18N
-        jLabel5.setText("jLabel5");
-
-        jLabel6.setText("jLabel6");
+        jLabel2 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Algerian", 2, 24)); // NOI18N
         jLabel1.setText("Biblioteca de Alejandria");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 26, -1, -1));
 
         jLabel3.setFont(new java.awt.Font("Cambria", 1, 18)); // NOI18N
-        jLabel3.setText(" USUARIO");
-
-        txt_User.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txt_UserActionPerformed(evt);
-            }
-        });
+        jLabel3.setText("USUARIO");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 215, 91, -1));
+        getContentPane().add(txt_User, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 249, 136, -1));
 
         jLabel4.setFont(new java.awt.Font("Cambria", 1, 18)); // NOI18N
-        jLabel4.setText("    CONTRASEÑA");
+        jLabel4.setText("CONTRASEÑA");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 291, 136, -1));
+        getContentPane().add(pass_Con, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 331, 136, -1));
 
         btn_Inicio.setText("INICIAR");
         btn_Inicio.addActionListener(new java.awt.event.ActionListener() {
@@ -63,71 +63,45 @@ public class Ventana_Uno extends javax.swing.JFrame {
                 btn_InicioActionPerformed(evt);
             }
         });
+        getContentPane().add(btn_Inicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(111, 392, 84, 47));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(118, 118, 118)
-                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(99, 99, 99)
-                        .addComponent(txt_User, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(96, 96, 96)
-                        .addComponent(pass_Con, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(120, 120, 120)
-                        .addComponent(btn_Inicio, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(90, 90, 90)
-                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(6, 6, 6)
-                        .addComponent(jLabel1)))
-                .addGap(22, 22, 22))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(246, 246, 246)
-                        .addComponent(jLabel3)
-                        .addGap(12, 12, 12)
-                        .addComponent(txt_User, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(356, 356, 356)
-                        .addComponent(pass_Con, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(396, 396, 396)
-                        .addComponent(btn_Inicio, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(322, 322, 322)
-                        .addComponent(jLabel4))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(60, 60, 60)
-                        .addComponent(jLabel1)))
-                .addGap(37, 37, 37))
-        );
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/image_Login.png"))); // NOI18N
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(118, 76, -1, -1));
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/image_Background.jpeg"))); // NOI18N
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 340, 490));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void txt_UserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_UserActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txt_UserActionPerformed
-
     private void btn_InicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_InicioActionPerformed
-        // TODO add your handling code here:
+          user = txt_User.getText().trim();
+          pass = pass_Con.getText().trim();
+          ResultSet rs = null;
+        if(!user.equals("") && !pass.equals("")){
+           
+            try {
+                Connection cn = Conexion.conectar();
+                PreparedStatement pst = cn.prepareStatement("select Usuario, Pass from usuarios where Usuario ='"+user+"' and Pass='"+pass+"'");
+                rs = pst.executeQuery();
+                if(rs.next()){
+                    System.out.println("LLEGA");
+                    dispose();
+                    new Ventana_Dos().setVisible(true);
+                }else{
+                    JOptionPane.showMessageDialog(null, "No se encuentra registrado");
+                }
+            } catch (SQLException e) {
+                System.out.println(e);
+                JOptionPane.showMessageDialog(null, "Error al comunicarse con el servidor");
+
+            }
+        }else{
+             JOptionPane.showMessageDialog(null, "Verifique que todos los espacios esten rellenos");
+        }
     }//GEN-LAST:event_btn_InicioActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -163,10 +137,10 @@ public class Ventana_Uno extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_Inicio;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JPasswordField pass_Con;
     private javax.swing.JTextField txt_User;
     // End of variables declaration//GEN-END:variables
